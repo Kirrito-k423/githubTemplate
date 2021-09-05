71c1cbdb954673950fa7bccbe7eefa2d1aeef2d9
supertan <943648187@qq.com>
2021-09-05 21:54:50 +0800

    ci: CI test2

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,14 +5,12 @@
  # @Author: Shaojie Tan
  # @Date: 2021-09-05 21:07:13
  # @LastEditors: Shaojie Tan
- # @LastEditTime: 2021-09-05 21:49:36
+ # @LastEditTime: 2021-09-05 21:53:28
 ### 
 # c++ travis CI
 set -e
 set -v
 pwd
-ls
-cd ..
 ls
 mkdir build
 cd build/
--- scripts/run.sh
+++ scripts/run.sh
@@ -5,10 +5,12 @@
  # @Author: Shaojie Tan
  # @Date: 2021-09-05 21:07:13
  # @LastEditors: Shaojie Tan
- # @LastEditTime: 2021-09-05 21:12:23
+ # @LastEditTime: 2021-09-05 21:54:13
 ### 
 # c++ travis CI
-
-cd ../build/bin/
+set -e
+set -v
+pwd
+cd build/bin/
 cp ../../src/{input_image.ppm,check.ppm} .
 ./SLIC

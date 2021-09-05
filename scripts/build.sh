4eb1be0f015256768615964e9ff24f7afa0774e5
supertan <943648187@qq.com>
2021-09-05 21:51:06 +0800

    build: CI test1

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,13 +5,18 @@
  # @Author: Shaojie Tan
  # @Date: 2021-09-05 21:07:13
  # @LastEditors: Shaojie Tan
- # @LastEditTime: 2021-09-05 21:11:21
+ # @LastEditTime: 2021-09-05 21:49:36
 ### 
 # c++ travis CI
-
+set -e
+set -v
+pwd
+ls
 cd ..
-cmake $@ .. 
+ls
+mkdir build
 cd build/
+cmake $@ .. 
 make
 
 

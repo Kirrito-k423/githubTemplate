fac65a48279374feaab8cfcbbe8832f6a7672c46
supertan <943648187@qq.com>
2021-09-07 10:59:39 +0800

    ci: add CODE_COVERAGE in CFLAGS

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,17 +5,20 @@
  # @Author: Shaojie Tan
  # @Date: 2021-09-05 21:07:13
  # @LastEditors: Shaojie Tan
- # @LastEditTime: 2021-09-05 21:53:28
+ # @LastEditTime: 2021-09-07 10:56:02
 ### 
 # c++ travis CI
 set -e
 set -v
 pwd
 ls
-mkdir build
-cd build/
-cmake $@ .. 
+mkdir -p build && cd build
+cmake $@ -DCODE_COVERAGE=ON .. 
+# Build (for Make on Unix equivalent to `make -j $(nproc)`)
+# cmake --build . --config Debug -- -j $(nproc)
 make
+# Test
+# ctest -j $(nproc) --output-on-failure
 
 
 

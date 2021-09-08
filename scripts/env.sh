d7fbb04abb9938f8b78ab704d390d0e7afe6be82
supertan <943648187@qq.com>
2021-09-08 10:49:57 +0800

    build: Install Boost & more output

--- scripts/env.sh
+++ scripts/env.sh
@@ -5,17 +5,25 @@
  # @Author: Shaojie Tan
  # @Date: 2021-09-07 22:41:26
  # @LastEditors: Shaojie Tan
- # @LastEditTime: 2021-09-07 22:45:27
+ # @LastEditTime: 2021-09-08 10:41:58
 ### 
 
 set -e
 set -v
 lscpu
 pwd
+aptitude search boost
 echo $Boost_INCLUDE_DIR
 echo $Boost_LIBRARYDIR
 echo $Boost_ROOT
 cd /
 ls
 cd /usr/bin
-ls 
+ls |grep oo
+cd /usr/local/include
+ls |grep oo
+cd /usr/local/lib
+ls |grep oo
+echo $PATH
+echo $CXX_INCLUDE_PATH
+echo $CXX_LIBRARY_PATH
